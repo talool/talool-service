5cf408330c8fbcd90f395bc9c20370f7d662579f
Chris Lintz <christopher_lintz@cable.comcast.com>
2013-02-28 22:17:06 -0700

    Thrift 9 code generation work + cocoa stubs

--- src/main/thrift/compile-thrift.sh
+++ src/main/thrift/compile-thrift.sh
@@ -6,9 +6,9 @@ scriptDir=$(dirname $0)
 
 #/opt/local/bin/thrift -v -o $scriptDir --gen java:java5 src/main/thrift/talool-service.thrift 
 
-/usr/local/bin/thrift -v -o $scriptDir --gen java:java5 src/main/thrift/talool-service.thrift 
+thrift -v -o $scriptDir --gen java:java5 src/main/thrift/talool-service.thrift 
 
-/usr/local/bin/thrift -v -o $scriptDir --gen cocoa src/main/thrift/talool-service.thrift
+thrift -v -o $scriptDir --gen cocoa src/main/thrift/talool-service.thrift
 
 cp -r  $scriptDir/gen-java/* src/main/java/.
 rm -rf $scriptDir/gen-java
