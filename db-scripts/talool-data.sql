8afe3911be8ccb2afe5256124d866449ceb1d290
Chris Lintz <clintz@Chriss-MacBook-Pro.local>
2013-03-14 00:34:37 -0600

    data import script

---
+++ db-scripts/talool-data.sql
@@ -0,0 +1,13 @@
+
+
+\connect talool
+
+INSERT INTO social_network(name, website, api_url)
+    VALUES ( 'Facebook', 'http://www.facebook.com', 'https://api.facebook.com');
+
+INSERT INTO social_network(name, website, api_url)
+
+    VALUES ( 'Twitter', 'http://www.twitter.com', 'https://api.twitter.com');
+ INSERT INTO social_network(name, website, api_url)
+
+    VALUES ( 'Pinterest', 'http://www.pinterest.com', 'https://api.pinterest.com');
