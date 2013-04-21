6db75fbc2817e98ae4d73e02c25634c38157f8b2
Chris Lintz <christopher.justin@gmail.com>
2013-04-21 14:34:39 -0600

    Seeded dealOffer/deal/dealPurchase

--- db-scripts/talool-data.sql
+++ db-scripts/talool-data.sql
@@ -82,9 +82,109 @@ INSERT INTO merchant_location (email,website_url,logo_url,phone,address_id)
        
 INSERT INTO merchant (primary_location_id,merchant_name)
        VALUES ((select merchant_location_id from merchant_location where email='info@centrolatinkitchen.com'),'Centro Latin Kitchen');
------------------------------------------------------------------------   
 
+---------Test Deal Offers & Deals -------------------------------   
 
+INSERT INTO deal_offer( merchant_id, created_by_merchant_account_id,updated_by_merchant_account_id, 
+                        deal_type,title,summary,code,price,is_active )
+        VALUES( (select merchant_id from merchant where merchant_name='Payback Book'),
+                (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+                (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+                'PAID_BOOK','Payback Book Test Book #1','Payback Book Test Book #1','payback-code12345','20.00',true);
+                
+INSERT INTO deal_offer( merchant_id, created_by_merchant_account_id,updated_by_merchant_account_id, 
+                        deal_type,title,summary,code,price,is_active )
+        VALUES( (select merchant_id from merchant where merchant_name='The Kitchen Boulder & [Upstairs]'),
+                (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+                (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+                'FREE_BOOK','The Kitchen Test Book #1','The Kitchen Test Book #1','kitchen-code12345','0.00',true);
+             
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='Payback Book Test Book #1'),
+               (select merchant_id from merchant where merchant_name='Centro Latin Kitchen'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               '1/2 Off Drinks', '1/2 Off Drinks', '1/2 Off Drinks','centro-12345','2013/12/29',true);
+               
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='Payback Book Test Book #1'),
+               (select merchant_id from merchant where merchant_name='Centro Latin Kitchen'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               '10 Bottles of Wine for $1', '10 Bottles of Wine for $1', '10 Bottles of Wine for $1','centro-baga','2013/12/29',true);
+               
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='Payback Book Test Book #1'),
+               (select merchant_id from merchant where merchant_name='The Kitchen Boulder & [Upstairs]'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               'Free Wine Tasting', 'Free Wine Tasting', 'Free Wine Tasting','kitchen-asdas','2015/11/20',true);
+               
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='Payback Book Test Book #1'),
+               (select merchant_id from merchant where merchant_name='The Kitchen Boulder & [Upstairs]'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               (select merchant_account_id from merchant_account where email='paybackbook@aol.com'),
+               '2 for 1 Lunch', '2 for 1 Lunch', '2 for 1 Lunch','kitchen-bdhdf','2015/11/20',true);    
 
+-------- Kitchen deal offers ----
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='The Kitchen Test Book #1'),
+               (select merchant_id from merchant where merchant_name='The Kitchen Boulder & [Upstairs]'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               'Free Garlic Fries', 'Free Garlic Fries with purchase of lunch or dinner', 'Free Garlic Fries with purchase of lunch or dinner',
+               'kitchen-881373','2013/12/29',true);
+               
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='The Kitchen Test Book #1'),
+               (select merchant_id from merchant where merchant_name='The Kitchen Boulder & [Upstairs]'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               '1/2 Off Wine', '1/2 Off Wine', '1/2 Off Wine',
+               'kitchen-87s1373','2013/12/29',true);
+               
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='The Kitchen Test Book #1'),
+               (select merchant_id from merchant where merchant_name='The Kitchen Boulder & [Upstairs]'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               'Free Drinks at Community Event','Free Drinks at Community Event', 'Free Drinks at Community Event',
+               'kitchen-651373','2013/12/29',true);
+               
+INSERT INTO deal (deal_offer_id,merchant_id,created_by_merchant_account_id,updated_by_merchant_account_id,
+                  title, summary, details, code, expires, is_active)
+       VALUES( (select deal_offer_id from deal_offer where title='The Kitchen Test Book #1'),
+               (select merchant_id from merchant where merchant_name='The Kitchen Boulder & [Upstairs]'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               (select merchant_account_id from merchant_account where email='merle@thekitchencafe.com'),
+               'Buy 1 Get 1 Free Lunch', 'Buy 1 Get 1 Free Lunch', 'Buy 1 Get 1 Free Lunch (M-F only)',
+               'kitchen-a1373','2013/12/29',true);
+                  
+
+------ Purchase Deals ------------
+
+INSERT INTO deal_offer_purchase (deal_offer_id,customer_id) 
+       VALUES ( (select deal_offer_id from deal_offer where title='The Kitchen Test Book #1'),  
+                (select customer_id from customer where email='christopher.justin@gmail.com'));
+
+INSERT INTO deal_offer_purchase (deal_offer_id,customer_id) 
+       VALUES ( (select deal_offer_id from deal_offer where title='Payback Book Test Book #1'),  
+                (select customer_id from customer where email='christopher.justin@gmail.com'));               
+
+INSERT INTO deal_offer_purchase (deal_offer_id,customer_id) 
+       VALUES ( (select deal_offer_id from deal_offer where title='The Kitchen Test Book #1'),  
+                (select customer_id from customer where email='douglasmccuen@gmail.com'));
+
+INSERT INTO deal_offer_purchase (deal_offer_id,customer_id)         
+        VALUES ( (select deal_offer_id from deal_offer where title='Payback Book Test Book #1'),  
+                 (select customer_id from customer where email='douglasmccuen@gmail.com')); 
 
 
