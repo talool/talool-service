b3db2d4903e466f7fe937b6347ab86901d0d6839
Chris Lintz <christopher.justin@gmail.com>
2013-07-09 09:55:40 -0600

    added ActivationSummary

---
+++ src/main/java/com/talool/core/ActivationSummary.java
@@ -0,0 +1,63 @@
+package com.talool.core;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * Simple Bean object for activation code summaries for a deal offer
+ * 
+ * @author clintz
+ * 
+ */
+public class ActivationSummary implements Serializable
+{
+	private static final long serialVersionUID = 355337005000488130L;
+	private UUID dealOfferId;
+	private String title;
+	private int totalCodes;
+	private int totalActivations;
+
+	public ActivationSummary()
+	{}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public int getTotalCodes()
+	{
+		return totalCodes;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public void setTotalCodes(int totalCodes)
+	{
+		this.totalCodes = totalCodes;
+	}
+
+	public UUID getDealOfferId()
+	{
+		return dealOfferId;
+	}
+
+	public void setDealOfferId(UUID dealOfferId)
+	{
+		this.dealOfferId = dealOfferId;
+	}
+
+	public int getTotalActivations()
+	{
+		return totalActivations;
+	}
+
+	public void setTotalActivations(int totalActivations)
+	{
+		this.totalActivations = totalActivations;
+	}
+
+}
