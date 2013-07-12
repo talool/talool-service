46cc96c8e5c8ca9f08cb2d6b015fceaee907b665
Chris Lintz <christopher.justin@gmail.com>
2013-07-11 21:42:03 -0600

    cascading customer removal - must be careful as deleting customers will delete their entire history

--- db-scripts/talool-schema-p1.sql
+++ db-scripts/talool-schema-p1.sql
@@ -55,3 +55,64 @@ CREATE INDEX activation_code_code_idx ON activation_code (code);
 
 CREATE INDEX activation_code_code_customer_id_idx ON activation_code (customer_id);
 
+BEGIN;
+ALTER TABLE customer_social_account DROP CONSTRAINT "FK_CustomerSocialAccount_Customer";
+ALTER TABLE customer_social_account ADD CONSTRAINT "FK_CustomerSocialAccount_Customer" 
+      FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
+COMMIT;
+
+BEGIN;
+ALTER TABLE deal_offer_purchase DROP CONSTRAINT "FK_DealOfferPurchase_Customer";
+ALTER TABLE deal_offer_purchase ADD CONSTRAINT "FK_DealOfferPurchase_Customer" 
+      FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
+COMMIT;
+
+BEGIN;
+ALTER TABLE deal_acquire DROP CONSTRAINT "FK_Dealacquire_Customer";
+ALTER TABLE deal_acquire ADD CONSTRAINT "FK_Dealacquire_Customer" 
+      FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
+COMMIT;
+
+BEGIN;
+ALTER TABLE deal_acquire_history DROP CONSTRAINT "FK_DealacquireHistory_Customer";
+ALTER TABLE deal_acquire_history ADD CONSTRAINT "FK_DealacquireHistory_Customer"
+      FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
+COMMIT;
+
+BEGIN;
+
+ALTER TABLE gift DROP CONSTRAINT "FK_Gift_Customer";
+ALTER TABLE gift ADD CONSTRAINT "FK_Gift_Customer"
+      FOREIGN KEY (from_customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
+COMMIT;
+
+BEGIN;
+ALTER TABLE deal_acquire_history DROP CONSTRAINT "FK_DealacquireHistory_Dealacquire";
+ALTER TABLE deal_acquire_history ADD CONSTRAINT "FK_DealacquireHistory_Dealacquire"
+      FOREIGN KEY (deal_acquire_id) REFERENCES deal_acquire(deal_acquire_id) ON DELETE CASCADE;
+COMMIT;
+
+
+BEGIN;
+ALTER TABLE gift DROP CONSTRAINT "FK_Gift_DealAcquire";
+ALTER TABLE gift ADD CONSTRAINT "FK_Gift_DealAcquire"
+      FOREIGN KEY (deal_acquire_id) REFERENCES deal_acquire(deal_acquire_id) ON DELETE CASCADE;
+COMMIT;
+
+BEGIN;
+ALTER TABLE deal_acquire DROP CONSTRAINT "FK_Dealacquire_Gift" ;
+ALTER TABLE deal_acquire ADD CONSTRAINT "FK_Dealacquire_Gift" 
+      FOREIGN KEY (gift_id) REFERENCES gift(gift_id) ON DELETE CASCADE;
+COMMIT;
+
+BEGIN;
+ALTER TABLE deal_acquire_history DROP CONSTRAINT "FK_DealacquireHistory_Gift" ;
+ALTER TABLE deal_acquire_history ADD CONSTRAINT "FK_DealacquireHistory_Gift" 
+      FOREIGN KEY (gift_id) REFERENCES gift(gift_id) ON DELETE CASCADE;
+COMMIT;
+
+DROP TABLE friend_request;
+
+DROP TABLE relationship;
+
+
