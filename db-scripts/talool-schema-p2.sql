84a996597daca4fae91cab1bcb303156b47a9d30
Chris Lintz <christopher.justin@gmail.com>
2013-09-09 22:50:12 -0600

    patch applied to dev and prod

---
+++ db-scripts/talool-schema-p2.sql
@@ -0,0 +1,18 @@
+
+
+-- cleaning up deleted users activities
+DELETE
+FROM  activity a where a.customer_id IN (
+SELECT distinct a.customer_id
+FROM    activity a
+LEFT JOIN customer c
+ON      c.customer_id = a.customer_id
+WHERE   c.customer_id IS NULL);
+
+
+-- now we can properly introduce a constraint that will cleanup activities when customer is deleted
+
+BEGIN;
+ALTER TABLE activity ADD CONSTRAINT "FK_Activity_Customer" 
+      FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE;
+COMMIT;
