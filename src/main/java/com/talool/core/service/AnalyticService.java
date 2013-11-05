668ce9c32894dfb4a473762b435e8281d1148c7f
Doug McCuen <doug@talool.com>
2013-11-05 10:51:59 -0700

    Adding new queries and objects for a merchant dashboard.

--- src/main/java/com/talool/core/service/AnalyticService.java
+++ src/main/java/com/talool/core/service/AnalyticService.java
@@ -1,5 +1,8 @@
 package com.talool.core.service;
 
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import com.talool.service.HibernateService;
@@ -27,5 +30,81 @@ public interface AnalyticService extends HibernateService
 	public Long getTotalFacebookCustomers() throws ServiceException;
 
 	public Long getTotalEmailCustomers() throws ServiceException;
+	
+	public List<AvailableDeal> getAvailableDeals(UUID merchantId) throws ServiceException;
+	
+	public List<RecentRedemption> getRecentRedemptions(UUID merchantId) throws ServiceException;
+	
+	public List<ActiveUser> getActiveUsers(UUID merchantId) throws ServiceException;
+	
+	public List<MerchantReach> getMerchantReaches(UUID merchantId) throws ServiceException;
+	
+	public class AvailableDeal implements Serializable {
+		private static final long serialVersionUID = 1L;
+		public String title;
+		public UUID dealId;
+		public Long redemptionCount;
+		public AvailableDeal(String title, UUID dealId, Long redemptionCount)
+		{
+			this.title = title;
+			this.dealId = dealId;
+			this.redemptionCount = redemptionCount;
+		}
+	}
+	
+	public class RecentRedemption implements Serializable {
+		private static final long serialVersionUID = 1L;
+		public String title;
+		public UUID dealId;
+		public String customerName;
+		public UUID customerId;
+		public String code;
+		public Date date;
+		public RecentRedemption(String title, UUID dealId, String cName, UUID cId, String c, Date d)
+		{
+			this.title = title;
+			this.dealId = dealId;
+			this.customerName = cName;
+			this.customerId = cId;
+			this.code = c;
+			this.date = d;
+		}
+	}
+	
+	public class ActiveUser implements Serializable {
+		private static final long serialVersionUID = 1L;
+		public String customerName;
+		public UUID customerId;
+		public Long dealCount;
+		public Long visitCount;
+		public Long messageCount;
+		public ActiveUser(String cName, UUID cId, Long d, Long v, Long m)
+		{
+			this.customerName = cName;
+			this.customerId = cId;
+			this.dealCount = d;
+			this.visitCount = v;
+			this.messageCount = m;
+		}
+	}
+	
+	public class MerchantReach implements Serializable {
+		private static final long serialVersionUID = 1L;
+		public String message;
+		public UUID messageId;
+		public Long quota;
+		public Long targetedUserCount;
+		public Long acquiredUserCount;
+		public MerchantReach(String m, UUID mid, Long q, Long t, Long a)
+		{
+			this.message = m;
+			this.messageId = mid;
+			this.quota = q;
+			this.targetedUserCount = t;
+			this.acquiredUserCount = a;
+		}
+	}
 
 }
+
+
--- src/main/java/com/talool/service/AnalyticServiceImpl.java
+++ src/main/java/com/talool/service/AnalyticServiceImpl.java
@@ -1,9 +1,13 @@
 package com.talool.service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.Query;
 import org.hibernate.SQLQuery;
+import org.hibernate.type.PostgresUUIDType;
 import org.hibernate.type.StandardBasicTypes;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -164,5 +168,188 @@ public class AnalyticServiceImpl extends AbstractHibernateService implements Ana
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public List<AvailableDeal> getAvailableDeals(UUID merchantId) throws ServiceException 
+	{
+		List<AvailableDeal> availableDeals= new ArrayList<AvailableDeal>();
+
+		try
+		{
+			final SQLQuery query = getCurrentSession().createSQLQuery(
+					"SELECT title AS t, d.deal_id AS did, count(*) AS cnt " +
+					"FROM deal AS d, deal_acquire AS da " +
+					"WHERE d.deal_id = da.deal_id AND d.merchant_id = :merchantId " +
+					"GROUP BY d.deal_id " +
+					"ORDER BY cnt DESC " +
+					"LIMIT 5");
+			
+			query.setParameter("merchantId", merchantId, PostgresUUIDType.INSTANCE);
+			query.addScalar("t", StandardBasicTypes.STRING);
+			query.addScalar("did", PostgresUUIDType.INSTANCE);
+			query.addScalar("cnt", StandardBasicTypes.LONG);
+			
+			
+			@SuppressWarnings("unchecked")
+			List<Object[]> l = query.list();
+
+			for (Object[] o : l)
+			{
+				String title = (String)o[0];
+				UUID id = (UUID)o[1];
+				Long cnt = (Long)o[2];
+				availableDeals.add(new AvailableDeal(title, id, cnt));
+			}
+
+		}
+		catch (Exception e)
+		{
+			throw new ServiceException(e.getLocalizedMessage(), e);
+		}
+		
+		return availableDeals;
+	}
+
+	@Override
+	public List<RecentRedemption> getRecentRedemptions(UUID merchantId) throws ServiceException 
+	{
+		List<RecentRedemption> rr = new ArrayList<RecentRedemption>();
+		
+		try
+		{
+			final SQLQuery query = getCurrentSession().createSQLQuery(
+					"SELECT title AS t, d.deal_id AS did, first_name AS fname, last_name AS lname, c.customer_id AS cid, redemption_code AS code, redemption_dt AS rdate " +
+					"FROM deal AS d, deal_acquire AS da, customer AS c " +
+					"WHERE d.deal_id = da.deal_id AND d.merchant_id = :merchantId AND da.customer_id = c.customer_id AND da.acquire_status = 'REDEEMED' " +
+					"ORDER BY rdate DESC " +
+					"LIMIT 5");
+			
+			query.setParameter("merchantId", merchantId, PostgresUUIDType.INSTANCE);
+			query.addScalar("t", StandardBasicTypes.STRING);
+			query.addScalar("did", PostgresUUIDType.INSTANCE);
+			query.addScalar("fname", StandardBasicTypes.STRING);
+			query.addScalar("lname", StandardBasicTypes.STRING);
+			query.addScalar("cid", PostgresUUIDType.INSTANCE);
+			query.addScalar("code", StandardBasicTypes.STRING);
+			query.addScalar("rdate", StandardBasicTypes.DATE);
+			
+			@SuppressWarnings("unchecked")
+			List<Object[]> l = query.list();
+
+			for (Object[] o : l)
+			{
+				String title = (String)o[0];
+				UUID id = (UUID)o[1];
+				String fname = (String)o[2];
+				String lname = (String)o[3];
+				String name = new StringBuilder(fname).append(" ").append(lname).toString();
+				UUID cId = (UUID)o[4];
+				String code = (String)o[5];
+				Date date = (Date)o[6];
+				rr.add(new RecentRedemption(title, id, name, cId, code, date));
+			}
+
+		}
+		catch (Exception e)
+		{
+			throw new ServiceException(e.getLocalizedMessage(), e);
+		}
+		
+		return rr;
+	}
+
+	@Override
+	public List<ActiveUser> getActiveUsers(UUID merchantId) throws ServiceException 
+	{
+		List<ActiveUser> au = new ArrayList<ActiveUser>();
+		
+		try
+		{
+			final SQLQuery query = getCurrentSession().createSQLQuery(
+					"SELECT first_name AS fname, last_name AS lname, c.customer_id AS uid, count(*) AS deals " +
+					"FROM deal AS d, deal_acquire AS da, customer AS c " +
+					"WHERE d.deal_id = da.deal_id AND d.merchant_id = :merchantId AND da.customer_id = c.customer_id AND da.acquire_status = 'REDEEMED' " +
+					"GROUP BY uid ORDER BY deals DESC " +
+					"LIMIT 5");
+			
+			query.setParameter("merchantId", merchantId, PostgresUUIDType.INSTANCE);
+			query.addScalar("fname", StandardBasicTypes.STRING);
+			query.addScalar("lname", StandardBasicTypes.STRING);
+			query.addScalar("uid", PostgresUUIDType.INSTANCE);
+			query.addScalar("deals", StandardBasicTypes.LONG);
+			//query.addScalar("visits", StandardBasicTypes.LONG);
+			//query.addScalar("messages", StandardBasicTypes.LONG);
+			
+			@SuppressWarnings("unchecked")
+			List<Object[]> l = query.list();
+
+			for (Object[] o : l)
+			{
+				String fname = (String)o[0];
+				String lname = (String)o[1];
+				String name = new StringBuilder(fname).append(" ").append(lname).toString();
+				UUID id = (UUID)o[2];
+				Long d = (Long)o[3];
+				//Long v = (Long)o[4];
+				//Long m = (Long)o[5];
+				au.add(new ActiveUser(name, id, d, d, d));
+			}
+
+		}
+		catch (Exception e)
+		{
+			throw new ServiceException(e.getLocalizedMessage(), e);
+		}
+		
+		return au;
+	}
+
+	@Override
+	public List<MerchantReach> getMerchantReaches(UUID merchantId) throws ServiceException 
+	{
+		// TODO Auto-generated method stub
+
+		List<MerchantReach> mr = new ArrayList<MerchantReach>();
+		
+		try
+		{
+			final SQLQuery query = getCurrentSession().createSQLQuery(
+					"SELECT title AS t, d.deal_id AS did, count(*) AS cnt " +
+					"FROM deal AS d, deal_acquire AS da " +
+					"WHERE d.deal_id = da.deal_id AND d.merchant_id = :merchantId " +
+					"GROUP BY d.deal_id " +
+					"ORDER BY cnt DESC " +
+					"LIMIT 5");
+			
+			query.setParameter("merchantId", merchantId, PostgresUUIDType.INSTANCE);
+			query.addScalar("t", StandardBasicTypes.STRING);
+			query.addScalar("did", PostgresUUIDType.INSTANCE);
+			query.addScalar("cnt", StandardBasicTypes.LONG);
+			
+			
+			@SuppressWarnings("unchecked")
+			List<Object[]> l = query.list();
+
+			for (Object[] o : l)
+			{
+				String message = (String)o[0];
+				UUID id = (UUID)o[1];
+				Long q = (Long)o[2];
+				Long t = (Long)o[3];
+				Long a = (Long)o[4];
+				mr.add(new MerchantReach(message, id, q, t, a));
+			}
+
+		}
+		catch (Exception e)
+		{
+			throw new ServiceException(e.getLocalizedMessage(), e);
+		}
+		
+		return mr;
+	}
+	
+	
+	
 
 }
