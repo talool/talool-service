081ce1c6fc89881299b02ca921eac59213b0411e
Chris Lintz <christopher.justin@gmail.com>
2014-01-15 23:57:23 -0700

    seed data / entertainment merchants createdByMerchantAccount createdByMerchant

--- db-scripts/talool-schema-1.0.7.sql
+++ db-scripts/talool-schema-1.0.7.sql
@@ -15,18 +15,81 @@ alter table merchant_location alter column created_by_merchant_account_id set no
 
 alter table merchant_location alter column created_by_merchant_id set not null;
 
+-- these updates go in pairs 
 update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='chris@entertainment.com')
  where merchant_id=(select merchant_id from merchant where merchant_name='!No Que No!');
  
 update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='chris@entertainment.com')
    where merchant_id=(select merchant_id from merchant where merchant_name='!No Que No!');
  
+----- 
 update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
  where merchant_id=(select merchant_id from merchant where merchant_name='A Hair Better');
  
 update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
    where merchant_id=(select merchant_id from merchant where merchant_name='A Hair Better');
 
+--
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='"Big" Mama''s');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name='"Big" Mama''s');
+   
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='"DEA" Music and Art School');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= '"DEA" Music and Art School');
+     
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='"Jacks" Are Wild');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= '"Jacks" Are Wild'); 
+   
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='Just Myrna');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= 'Just Myrna'); 
+
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='Let''s Paint - Fun & Easy Art');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= 'Let''s Paint - Fun & Easy Art');  
+
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='"Time-Out!" Dance');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= '"Time-Out!" Dance');    
+
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='#1 Nails');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= '#1 Nails');    
+   
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='$1 $tore');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= '$1 $tore');    
+   
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='$5 Crunch');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= '$5 Crunch');    
+
+update merchant_location set created_by_merchant_account_id=(select merchant_account_id from merchant_account where email='doug@entertainment.com')
+ where merchant_id=(select merchant_id from merchant where merchant_name='$5 Car Wash');
+ 
+update merchant_location set created_by_merchant_id=(select merchant_id from merchant_account where email='doug@entertainment.com')
+   where merchant_id=(select merchant_id from merchant where merchant_name= '$5 Car Wash');      
+   
 update deal_offer set geom=ST_GeomFromText('POINT(-105.2797 40.0176)', 4326) 
   where title='Entertainment Book';
     
