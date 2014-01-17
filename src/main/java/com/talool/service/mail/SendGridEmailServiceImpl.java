1927da8e8ef6672a585fbce520d55e82848ffe52
Doug McCuen <doug@talool.com>
2014-01-17 13:54:49 -0700

    * new EmailService implementation for SendGrid

---
+++ src/main/java/com/talool/service/mail/SendGridEmailServiceImpl.java
@@ -0,0 +1,140 @@
+package com.talool.service.mail;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.sendgrid.SendGrid;
+import com.talool.core.Customer;
+import com.talool.core.gift.EmailGift;
+import com.talool.core.service.EmailService;
+import com.talool.core.service.ServiceException;
+import com.talool.service.ErrorCode;
+import com.talool.service.ServiceConfig;
+
+import freemarker.template.TemplateException;
+
+public class SendGridEmailServiceImpl implements EmailService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SendGridEmailServiceImpl.class);
+
+	private static SendGridEmailServiceImpl instance;
+	
+	private final String sgUsername;
+	private final String sgPassword;
+
+	private SendGridEmailServiceImpl()
+	{
+		// TODO move this to the service config
+		sgUsername = "vince@talool.com";
+		sgPassword = "321Abc990";
+	};
+
+	public static synchronized SendGridEmailServiceImpl createInstance() throws IOException
+	{
+		if (instance == null)
+		{
+			instance = new SendGridEmailServiceImpl();
+		}
+
+		return instance;
+	}
+	
+	@Override
+	public void sendEmail(String subject, String recipient, String from,
+			String messageBody) throws ServiceException {
+		
+		SendGrid sendgrid = new SendGrid(sgUsername, sgPassword);
+
+		sendgrid.addTo(recipient);
+		//sendgrid.addToName("");
+		sendgrid.setFrom(from);
+		//sendgrid.addFromName("");
+		sendgrid.setSubject(subject);
+		sendgrid.setHtml(messageBody);
+
+		sendgrid.send();
+	}
+	
+	@Override
+	public void sendCustomerRegistrationEmail(Customer customer)
+			throws ServiceException {
+		try
+		{
+			sendEmail(ServiceConfig.get().getRegistrationSubj(), customer.getEmail(), ServiceConfig.get().getMailFrom(),
+					FreemarkerUtil.get().renderRegistrationEmail(customer));
+
+			if (LOG.isDebugEnabled())
+			{
+				LOG.debug("Registration email successfully sent to " + customer.getEmail());
+			}
+
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			throw new ServiceException(e.getLocalizedMessage(), e);
+		}
+		catch (TemplateException e)
+		{
+			e.printStackTrace();
+			throw new ServiceException(ErrorCode.MAIL_TEMPLATE_NOT_FOUND, e);
+		}
+
+	}
+
+	@Override
+	public void sendPasswordRecoveryEmail(Customer customer)
+			throws ServiceException {
+		try
+		{
+			sendEmail(ServiceConfig.get().getPasswordRecoverySubj(), customer.getEmail(), ServiceConfig.get().getMailFrom(),
+					FreemarkerUtil.get().renderPasswordRecoveryEmail(customer));
+
+			if (LOG.isDebugEnabled())
+			{
+				LOG.debug("Password recovery email successfully sent to " + customer.getEmail());
+			}
+
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			throw new ServiceException(e.getLocalizedMessage(), e);
+		}
+		catch (TemplateException e)
+		{
+			e.printStackTrace();
+			throw new ServiceException(ErrorCode.MAIL_TEMPLATE_NOT_FOUND, e);
+		}
+
+	}
+
+	@Override
+	public void sendGiftEmail(EmailGift gift) throws ServiceException {
+		try
+		{
+			sendEmail(ServiceConfig.get().getGiftSubj(), gift.getToEmail(), ServiceConfig.get().getMailFrom(),
+					FreemarkerUtil.get().renderGiftEmail(gift));
+
+			if (LOG.isDebugEnabled())
+			{
+				LOG.debug("Gift email successfully sent to " + gift.getToEmail());
+			}
+
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			throw new ServiceException(e.getLocalizedMessage(), e);
+		}
+		catch (TemplateException e)
+		{
+			e.printStackTrace();
+			throw new ServiceException(ErrorCode.MAIL_TEMPLATE_NOT_FOUND, e);
+		}
+	}
+	
+
+}
