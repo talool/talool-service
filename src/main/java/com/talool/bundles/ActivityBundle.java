699f5b03a52aa4f7dc0c05fd534e65261b1c2dba
Chris Lintz <christopher.justin@gmail.com>
2014-11-30 15:59:43 -0700

    acquired for free activity message

--- src/main/java/com/talool/bundles/ActivityBundle.java
+++ src/main/java/com/talool/bundles/ActivityBundle.java
@@ -1,49 +1,50 @@
 package com.talool.bundles;
 
-public final class ActivityBundle
-{
-	public static final String SENT_FACEBOOK_GIFT_TITLE = "sent.facebook.gift.title";
-	public static final String SENT_FACEBOOK_GIFT_SUBTITLE = "sent.facebook.gift.subtitle";
-	public static final String RECV_FACEBOOK_GIFT_TITLE = "recv.facebook.gift.title";
-	public static final String RECV_FACEBOOK_GIFT_SUBTITLE = "recv.facebook.gift.subtitle";
+public final class ActivityBundle {
+  public static final String SENT_FACEBOOK_GIFT_TITLE = "sent.facebook.gift.title";
+  public static final String SENT_FACEBOOK_GIFT_SUBTITLE = "sent.facebook.gift.subtitle";
+  public static final String RECV_FACEBOOK_GIFT_TITLE = "recv.facebook.gift.title";
+  public static final String RECV_FACEBOOK_GIFT_SUBTITLE = "recv.facebook.gift.subtitle";
 
-	public static final String SENT_EMAIL_GIFT_TITLE = "sent.email.gift.title";
-	public static final String SENT_EMAIL_GIFT_SUBTITLE = "sent.email.gift.subtitle";
-	public static final String RECV_EMAIL_GIFT_TITLE = "recv.email.gift.title";
-	public static final String RECV_EMAIL_GIFT_SUBTITLE = "recv.email.gift.subtitle";
+  public static final String SENT_EMAIL_GIFT_TITLE = "sent.email.gift.title";
+  public static final String SENT_EMAIL_GIFT_SUBTITLE = "sent.email.gift.subtitle";
+  public static final String RECV_EMAIL_GIFT_TITLE = "recv.email.gift.title";
+  public static final String RECV_EMAIL_GIFT_SUBTITLE = "recv.email.gift.subtitle";
 
-	public static final String PURCHASED_DEAL_OFFER_TITLE = "purchased.dealoffer.title";
+  public static final String PURCHASED_DEAL_OFFER_TITLE = "purchased.dealoffer.title";
 
-	public static final String ACTIVATED_DEAL_OFFER_TITLE = "activated.dealoffer.title";
+  public static final String ACQUIRED_FREE_DEAL_OFFER_TITLE = "acquired.free.dealoffer.title";
 
-	public static final String FRIEND_ACCEPTED_DEAL_TITLE = "friend.accepted.gift.title";
-	public static final String FRIEND_ACCEPTED_DEAL_SUBTITLE = "friend.accepted.gift.subtitle";
+  public static final String ACTIVATED_DEAL_OFFER_TITLE = "activated.dealoffer.title";
 
-	public static final String FRIEND_REDEEMED_DEAL_TITLE = "friend.redeemed.deal.title";
-	public static final String FRIEND_REDEEMED_DEAL_SUBTITLE = "friend.redeemed.deal.subtitle";
+  public static final String FRIEND_ACCEPTED_DEAL_TITLE = "friend.accepted.gift.title";
+  public static final String FRIEND_ACCEPTED_DEAL_SUBTITLE = "friend.accepted.gift.subtitle";
 
-	public static final String FRIEND_REJECTED_DEAL_TITLE = "friend.rejected.gift.title";
-	public static final String FRIEND_REJECTED_DEAL_SUBTITLE = "friend.rejected.gift.subtitle";
+  public static final String FRIEND_REDEEMED_DEAL_TITLE = "friend.redeemed.deal.title";
+  public static final String FRIEND_REDEEMED_DEAL_SUBTITLE = "friend.redeemed.deal.subtitle";
 
-	public static final String ACCEPTED_DEAL_TITLE = "accepted.gift.title";
-	public static final String ACCEPTED_DEAL_SUBTITLE = "accepted.gift.subtitle";
+  public static final String FRIEND_REJECTED_DEAL_TITLE = "friend.rejected.gift.title";
+  public static final String FRIEND_REJECTED_DEAL_SUBTITLE = "friend.rejected.gift.subtitle";
 
-	public static final String REDEEMED_DEAL_TITLE = "redeemed.deal.title";
-	public static final String REDEEMED_DEAL_SUBTITLE = "redeemed.deal.subtitle";
+  public static final String ACCEPTED_DEAL_TITLE = "accepted.gift.title";
+  public static final String ACCEPTED_DEAL_SUBTITLE = "accepted.gift.subtitle";
 
-	public static final String REJECTED_GIFT_TITLE = "rejected.gift.title";
-	public static final String REJECTED_GIFT_SUBTITLE = "rejected.gift.subtitle";
+  public static final String REDEEMED_DEAL_TITLE = "redeemed.deal.title";
+  public static final String REDEEMED_DEAL_SUBTITLE = "redeemed.deal.subtitle";
 
-	public static final String WELCOME_TITLE = "welcome.title";
-	public static final String WELCOME_SUBTITLE = "welcome.subtitle";
+  public static final String REJECTED_GIFT_TITLE = "rejected.gift.title";
+  public static final String REJECTED_GIFT_SUBTITLE = "rejected.gift.subtitle";
 
-	public static final String WELCOME_GIFTS_TITLE = "welcome.gifts.title";
-	public static final String WELCOME_GIFTS_SUBTITLE = "welcome.gifts.subtitle";
+  public static final String WELCOME_TITLE = "welcome.title";
+  public static final String WELCOME_SUBTITLE = "welcome.subtitle";
 
-	public static final String FUNDRAISER_SUPPORT_TITLE = "fundraiser.support.title";
-	public static final String FUNDRAISER_SUPPORT_SUBTITLE = "fundraiser.support.subtitle";
+  public static final String WELCOME_GIFTS_TITLE = "welcome.gifts.title";
+  public static final String WELCOME_GIFTS_SUBTITLE = "welcome.gifts.subtitle";
 
-	public static final String GIFT_RETURNED_EMAIL_DOES_NOT_EXIST_TITLE = "gift.returned.email.does.not.exist.title";
-	public static final String GIFT_RETURNED_EMAIL_DOES_NOT_EXIST_SUBTITLE = "gift.returned.email.does.not.exist.subtitle";
+  public static final String FUNDRAISER_SUPPORT_TITLE = "fundraiser.support.title";
+  public static final String FUNDRAISER_SUPPORT_SUBTITLE = "fundraiser.support.subtitle";
+
+  public static final String GIFT_RETURNED_EMAIL_DOES_NOT_EXIST_TITLE = "gift.returned.email.does.not.exist.title";
+  public static final String GIFT_RETURNED_EMAIL_DOES_NOT_EXIST_SUBTITLE = "gift.returned.email.does.not.exist.subtitle";
 
 }
